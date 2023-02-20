4a67be02d34ee4d7c9288a95f4716924d7f09f76
rahul <rahul@TIKUDELL-2011>
2023-02-20 11:28:41 +0530

    check 5

---
+++ general/src/main/java/general/Mko.java
@@ -0,0 +1,46 @@
+package general;
+
+
+public class Mko {
+	public static void main(String[] args) {
+		int a= 5;
+		
+			       
+
+			
+//				
+////				System.out.println( objects2.b);
+//				System.out.println("r.b = "+ r.b);
+//				System.out.println("m.b = "+ m.b);
+		System.out.println("hello");
+		System.out.println("rahul");
+
+
+//				
+//				System.out.println("=====non static======");
+//				m.b=45;                                    // non static
+//				
+//				System.out.println("r.b = "+ r.b);
+//				System.out.println("m.b = "+ m.b);
+//				
+//				System.out.println("===========");
+//				
+//				System.out.println( objects2.a);
+//				System.out.println (r.a);
+//				System.out.println ( m.a);
+//				
+//		        System.out.println("===== static======");
+//		        
+//		        r.a= 100;                                   //static
+//		        
+//		    	System.out.println( objects2.a);
+//				System.out.println (r.a);
+//				System.out.println ( m.a);
+//		        
+//				System.out.println("===========");
+//				
+				
+		
+	}
+
+}
